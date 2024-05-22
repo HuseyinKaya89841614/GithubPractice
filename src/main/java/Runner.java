c88c519b26055ff5b9acb1929c09e477d9f44988
Huseyin <93405452+HuseyinKaya89841614@users.noreply.github.com>
2024-05-22 21:05:06 +0200

    hsy3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,5 +2,8 @@ public class Runner {
     public static void main(String[] args) {
 //github reposunda bir degisiklik
 //huseyin reposunda bir degisiklik yaptik
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
